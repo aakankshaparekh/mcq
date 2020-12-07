454c50da3eecd9feabd370008bb0520c9814d2ed
aakankshaparekh <67599363+aakankshaparekh@users.noreply.github.com>
2020-12-07 13:37:27 +0530

    Update subjects.js

--- subjects.js
+++ subjects.js
@@ -1,34 +1,38 @@
 var subjects = [
+  // {
+  //   code: 66,
+  //   name: "Cost Accounting",
+  // },
+  // {
+  //   code: 404,
+  //   name: "Innovation Management",
+  // },
+  // {
+  //   code: 408,
+  //   name: "Strategic Management",
+  // },
+  // {
+  //   code: 526,
+  //   name: "Financial Management I",
+  // },
+  // {
+  //   code: 605,
+  //   name: "Financial Management II",
+  // },
+  // {
+  //   code: 629,
+  //   name: "Trade Finance",
+  // },
+  // {
+  //   code: 661,
+  //   name: "Advance Direct Tax",
+  // },
+  // {
+  //   code: 662,
+  //   name: "GST-The Essentials of Goods and Services Tax",
+  // },
   {
-    code: 66,
-    name: "Cost Accounting",
-  },
-  {
-    code: 404,
-    name: "Innovation Management",
-  },
-  {
-    code: 408,
-    name: "Strategic Management",
-  },
-  {
-    code: 526,
-    name: "Financial Management I",
-  },
-  {
-    code: 605,
-    name: "Financial Management II",
-  },
-  {
-    code: 629,
-    name: "Trade Finance",
-  },
-  {
-    code: 661,
-    name: "Advance Direct Tax",
-  },
-  {
-    code: 662,
-    name: "GST-The Essentials of Goods and Services Tax",
+    code: 315,
+    name: "International Finance",
   },
 ];
